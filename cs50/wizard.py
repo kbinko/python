50913b9bd105e6869018cda740048fc9f7e957f8
Kbinko <59764236+kbinko@users.noreply.github.com>
2023-11-27 11:06:28 +0000

    .

--- cs50/wizard.py
+++ cs50/wizard.py
@@ -1,14 +1,20 @@
-class Student:
-    def __init__(self, name, house):
+class Wizard:
+    def __init__(self, name):
         if not name:
             raise ValueError("Name cannot be empty")
         self.name = name
+
+class Student(Wizard):
+    def __init__(self, name, house):
+        super().__init__(name)
         self.house = house
 
 
-class Professor:
+class Professor(Wizard):
     def __init__(self, name, subject):
-        if not name:
-            raise ValueError("Name cannot be empty")
-        self.name = name
+        super().__init__(name)
         self.subject = subject
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Potions")
