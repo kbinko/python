4a96c90b6acebb1523ea7935d433c529ff4e0965
Kbinko <59764236+kbinko@users.noreply.github.com>
2023-11-27 10:05:15 +0000

    .

--- cs50/oop.py
+++ cs50/oop.py
@@ -2,20 +2,31 @@ class Student:
     def __init__(self, name, house):
         if not name:
             raise ValueError("Name is required")
-        if house not in ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
     def __str__(self):
         return f"{self.name} from {self.house}"
     
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+    
 def main():
     student = get_student()
-    print(student)    
+    print(student)  
+ 
     
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+
     try:
         return Student(name, house)
     except ValueError as error:
